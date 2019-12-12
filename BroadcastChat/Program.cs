e910e576c2feacda5fcf916363e224399d8d8861
Nokia3310Team <icake.cc@gmail.com>
2019-12-12 22:14:07 +0100

    henrikglöminte
    Du kan använda dessa filer och kolla hur programmet fungerar. Snälla skriv wpf till appen klart till nästa vecka // Artur

---
+++ BroadcastChat/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace BroadcastChat
+{
+    class Program
+    {
+        //port som client lyssnar på
+        private const int ListenPort = 8080;
+
+        static void Main(string[] args)
+        {
+            //ip addressen som man skickar förfrågor, man kan ändra den så den stämmer med din local nätverk ip
+            // man kan kolla detta i terminal men commanden ipconfig
+            IPAddress address = IPAddress.Parse("192.168.80.58");
+            //skapar socket
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+                SocketType.Dgram, ProtocolType.Udp);
+            socket.EnableBroadcast = true;
+
+            //den behövs inte just nu
+            UdpClient Sendback = new UdpClient(8081);
+            IPEndPoint ep = new IPEndPoint(address, ListenPort);
+
+
+            while (true)
+            {
+                //där man kan skriva medelande, omvandla den till bytes och skicka den till server
+                Console.Write(">");
+                string msg = Console.ReadLine();
+                
+                byte[] sendbuf = Encoding.UTF8.GetBytes(msg);
+                socket.SendTo(sendbuf, ep);
+
+            }
+        }
+
+        
+    }
+}
---
+++ Brodder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Brodder
+{
+    class Program
+    {
+        //port som man lyssnar på
+        private const int ListenPort = 8080;
+
+        static void Main(string[] args)
+        {
+            //skapar threads
+            var listenThread = new Thread(Listener);
+            listenThread.Start();
+
+        }
+
+        static void Listener()
+        {
+            //skapar ny instans av udpclient och socket
+            UdpClient listener = new UdpClient(ListenPort);
+            Socket socket = new Socket(AddressFamily.InterNetwork,
+               SocketType.Dgram, ProtocolType.Udp);
+            socket.EnableBroadcast = true;
+
+            try
+            {
+                while (true)
+                {
+                    //den lyssnar på portet och alla ip addressana
+                    IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, ListenPort);
+                    //få information i bytes
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    //skriver ut data och omvandlar den till utf8
+                    Console.WriteLine("Received broadcast from {0} : {1}\n", groupEP.ToString(),
+                        Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+    }
+}
